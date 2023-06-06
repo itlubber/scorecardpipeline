b8bc4cd8f5fb98c6caedcadbf16cd586db76afd8
itlubber <39397838+itlubber@users.noreply.github.com>
2023-06-06 09:15:38 +0800

    fix import *

--- scorecardpipeline/__init__.py
+++ scorecardpipeline/__init__.py
@@ -4,10 +4,10 @@
 @Author  : itlubber
 @Site    : itlubber.art
 """
-from sklearn.pipeline import Pipeline
 from toad.metrics import KS, AUC, F1, PSI
 from openpyxl.formatting.rule import ColorScaleRule
 from openpyxl.utils import get_column_letter, column_index_from_string
+from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline, make_union
 
 from .logger import init_logger
 from .utils import *
@@ -16,16 +16,15 @@ from .model import ITLubberLogisticRegression, ScoreCard
 from .excel_writer import ExcelWriter
 
 
-__version__ = "0.1.8"
-
+__version__ = "0.1.9"
 __all__ = (
-    __version__
-    , FeatureSelection, FeatureImportanceSelector, StepwiseSelection, Combiner, WOETransformer
-    , ITLubberLogisticRegression, ScoreCard
-    , Pipeline, KS, AUC, PSI
-    , init_logger, init_setting, load_pickle, save_pickle, germancredit
-    , ColorScaleRule, get_column_letter, column_index_from_string, seed_everything
-    , feature_bins, feature_bin_stats
-    , bin_plot, corr_plot, ks_plot, hist_plot, psi_plot, dataframe_plot, distribution_plot
-    , ExcelWriter
+    "__version__"
+    , "FeatureSelection", "FeatureImportanceSelector", "StepwiseSelection", "Combiner", "WOETransformer"
+    , "ITLubberLogisticRegression", "ScoreCard"
+    , "Pipeline", "KS", "AUC", "PSI", "F1", "FeatureUnion", "make_pipeline", "make_union"
+    , "init_logger", "init_setting", "load_pickle", "save_pickle", "germancredit"
+    , "ColorScaleRule", "get_column_letter", "column_index_from_string", "seed_everything"
+    , "feature_bins", "feature_bin_stats"
+    , "bin_plot", "corr_plot", "ks_plot", "hist_plot", "psi_plot", "dataframe_plot", "distribution_plot"
+    , "ExcelWriter"
 )
