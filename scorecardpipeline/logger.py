793ee47b03f3b2c7405adbc3189c08a7f308739f
itlubber <1830611168@qq.com>
2026-03-29 23:56:39 +0800

    Revert "Update logger.py with performance optimizations."
    This reverts commit 9d669e4b22e95ff10e4003f230317cc76d863704.

--- scorecardpipeline/logger.py
+++ scorecardpipeline/logger.py
@@ -1,23 +1,49 @@
 import os
+import sys
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO, ERROR
 
-CACHE_DIRNAME = os.path.dirname(__file__)
 
-# Assuming the previous implementation had certain operations repeated;
-# Optimization in exception message formatting and handler management
+def init_logger(filename=None, stream=True, fmt="[ %(asctime)s ][ %(levelname)s ][ %(filename)s:%(funcName)s:%(lineno)d ] %(message)s", datefmt=None):
+    """
+    初始化日志
 
-def log_error(exception):
-    """Log the error with formatted exception details."""
-    print(f'Error occurred: {str(exception)}')  # Example of formatted message
+    :param filename: 日志文件存储地址，如果不传不记录日志到文件中，默认为 None
+    :param stream: 是否显示在终端中，默认 True
+    :param fmt: 日志格式，参考：https://docs.python.org/3/library/logging.html#formatter-objects
+    :param datefmt: 日期格式
+    :return: logging.Logger
+    """
+    logger = getLogger("scorecardpipeline")
+    logger.setLevel(DEBUG)
+    formatter = Formatter(fmt, datefmt=datefmt)
 
-class Logger:
-    def __init__(self):
-        self.handlers = []  # Improved handler management
+    if filename:
+        if os.path.dirname(filename) != "" and not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except Exception as error:
+                print(f'错误 >> 创建日志目录失败,清手动创建目录文件位置,运行 sudo mkdir -p {os.path.dirname(filename)}')
+                print('错误 >> 报错信息 : {}'.format(error))
 
-    def add_handler(self, handler):
-        self.handlers.append(handler)
+        # fh = TimedRotatingFileHandler(filename=filename, when='D', backupCount=30, encoding="utf-8")
+        fh = RotatingFileHandler(filename=filename, mode='a', maxBytes=10*1024**2, backupCount=0, encoding="utf-8")
+        fh.setLevel(INFO)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        fh.close()
+    
+    if stream:
+        ch = StreamHandler(sys.stdout)
+        ch.setLevel(INFO)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        ch.close()
 
-    def log(self, message):
-        for handler in self.handlers:
-            handler.handle(message)
+    return logger
 
-# Add other methods and implementations as needed
+
+if __name__ == '__main__':
+    logger = init_logger()
+    logger.info(type(logger))
+    logger.info("import scorecardpipeline as sp")
